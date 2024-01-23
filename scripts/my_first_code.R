efb35acfd8137d2f57f2993194126c2a9f11764e
Armi Enriquez <asenriquez2@up.edu.ph>
2024-01-23 22:52:54 +0800

    added more basic coding

--- scripts/my_first_code.R
+++ scripts/my_first_code.R
@@ -8,4 +8,23 @@ my_weight_kg
 my_weight <- 50
 2.2*weight_kg
 weight_lb <- 2.2*weight_kg
+# I have to update the value of the weight_kg object
 weight_kg <- 100
+weight_lb
+mass <- 47.5
+age <- 122
+mass <- mass * 2.0
+age <- age - 20
+mass_index <- mass/age
+sqrt(9)
+?round()
+round(3.14159)
+round(3.14159, digits=2)
+round(3.14159,2)
+round(digits=2,x=3.14159)
+round(2,3.14159)
+my_function <- function(x){
+  x<-x+2
+}
+my_result <- my_function(3)
+}
