9d5e47d953bd3c537af17dc09eadfa8375f851a9
Armi Enriquez <asenriquez2@up.edu.ph>
2024-01-24 17:07:32 +0800

    Learned about factors

--- scripts/third_data_frame_scripts.R
+++ scripts/third_data_frame_scripts.R
@@ -55,3 +55,54 @@ surveys_200_lec <- surveys[200,]
 nrow(surveys)
 
 lastrow <- surveys[nrow(surveys),]
+
+#Factors
+str(surveys)
+surveys$sex <- factor(surveys$sex)
+
+#What are the levels of sex
+levels(surveys$sex)
+
+#How many levels
+nlevels(surveys$sex)
+
+#Force the assignment instead of alphabetical
+sex <- factor(c ("male", "female", "female", "male"))
+sex <- factor(sex, levels = c("male", "female"))
+
+surveys$taxa <- factor(surveys$taxa)
+surveys$genus <- factor(surveys$genus)
+
+nlevels(surveys$taxa)
+nlevels(surveys$genus)
+
+sum(surveys$taxa=="Rabbit")
+summary(surveys)
+
+# Convert factors
+as.character(sex)
+
+year_fct <-  factor(c(1990, 1983, 1977, 1997))
+as.numeric(year_fct)
+
+as.numeric(as.character(year_fct))
+
+as.numeric(levels(year_fct))
+
+#Renaming factors
+plot(surveys$sex)
+summary(surveys$sex)
+sex <- surveys$sex
+levels(sex)
+sex <- addNA(sex)
+levels(sex)
+
+levels(sex)[3] <- "undetermined"
+levels(sex)
+sex
+
+plot(sex)
+levels(sex)[1:2] <- c("female","male")
+plot(sex)
+
+sex <- factor(sex,levels=c("undetermined", "female", "male"))
